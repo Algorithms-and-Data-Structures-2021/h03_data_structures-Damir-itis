9f46a307bbd449a991a9ff6a1ad7192c26854606
Damir <damir20020711@mail.ru>
2021-05-30 15:28:50 +0300

    The end

--- src/array_stack.cpp
+++ src/array_stack.cpp
@@ -3,74 +3,110 @@
 #include <algorithm>  // copy, fill
 #include <cassert>    // assert
 #include <stdexcept>  // logic_error, invalid_argument
+#include <vector>
+
+using namespace std;
 
 namespace itis {
 
-ArrayStack::ArrayStack(int capacity) {
-  if (capacity <= 0) {
-    throw std::invalid_argument("initial capacity must be greater than zero");
-  }
+    ArrayStack::ArrayStack(int capacity) {
+        if (capacity <= 0) {
+            throw std::invalid_argument("initial capacity must be greater than zero");
+        }
+        // TODO: напишите здесь свой код ...
+        Element* array = new Element[capacity];
+        for (int i = 0; i < capacity; i ++) {
+            array[i] = Element::UNDEFINED;
+        }
+        capacity_ = capacity;
+        data_ = array;
 
-  // TODO: напишите здесь свой код ...
-}
+    }
 
-ArrayStack::~ArrayStack() {
-  // TODO: напишите здесь свой код ...
-}
+    ArrayStack::~ArrayStack() {
+        // TODO: напишите здесь свой код ...
+        delete[] data_;
+        data_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
+    }
 
-void ArrayStack::Push(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+    void ArrayStack::Push(Element e) {
+        // TODO: напишите здесь свой код ...
+        if (size_ == capacity_) {
+            resize(capacity_ + kCapacityGrowthCoefficient);
+        }
+        data_[size_] = e;
+        size_ ++;
+    }
 
-void ArrayStack::Pop() {
-  if (size_ == 0) {
-    throw std::logic_error("cannot pop out from empty stack");
-  }
+    void ArrayStack::Pop() {
+        if (size_ == 0) {
+            throw std::logic_error("cannot pop out from empty stack");
+        }
+        data_[size_-1] = Element::UNDEFINED;
+        size_--;
+        // TODO: напишите здесь свой код ...
 
-  // TODO: напишите здесь свой код ...
-}
+    }
 
-void ArrayStack::Clear() {
-  // TODO: напишите здесь свой код ...
-}
+    void ArrayStack::Clear() {
+        // TODO: напишите здесь свой код ...
+        for (int  i =0; i < size_; i ++) {
+            data_[i] = Element::UNDEFINED;
+        }
+        size_ = 0;
+    }
 
-void ArrayStack::resize(int new_capacity) {
-  assert(new_capacity > size_);
+    void ArrayStack::resize(int new_capacity) {
+        assert(new_capacity > size_);
+        if (new_capacity > capacity_) {
+            Element* new_array = new Element[new_capacity];
+            // TODO: напишите здесь свой код ...
+            for (int i = 0; i < size_; i++) {
+                new_array[i] = data_[i];
+            }
+            std::fill(new_array + size_, new_array + capacity_, Element::UNDEFINED);
+            delete[] data_;
+            data_ = new_array;
+            capacity_ = new_capacity;
+        }
 
-  // TODO: напишите здесь свой код ...
-}
+    }
+
+
 
 // === РЕАЛИЗОВАНО ===
 
-std::optional<Element> ArrayStack::Peek() const {
-  // возвращаем вершину стека или ничего (nullopt), если стек пустой
-  return size_ == 0 ? std::nullopt : std::make_optional(data_[size_ - 1]);
-}
+    std::optional<Element> ArrayStack::Peek() const {
+        // возвращаем вершину стека или ничего (nullopt), если стек пустой
+        return size_ == 0 ? std::nullopt : std::make_optional(data_[size_ - 1]);
+    }
 
-bool ArrayStack::IsEmpty() const {
-  return size_ == 0;
-}
+    bool ArrayStack::IsEmpty() const {
+        return size_ == 0;
+    }
 
-int ArrayStack::size() const {
-  return size_;
-}
+    int ArrayStack::size() const {
+        return size_;
+    }
 
-int ArrayStack::capacity() const {
-  return capacity_;
-}
+    int ArrayStack::capacity() const {
+        return capacity_;
+    }
 
 // === НЕОБХОДИМО ДЛЯ ТЕСТИРОВАНИЯ ===
 
-std::ostream &operator<<(std::ostream &os, const ArrayStack &stack) {
-  os << "size: " << stack.size_ << '\n';
-  if (stack.data_ != nullptr) {
-    // выводим элементы сверху вниз
-    for (int index = stack.size_ - 1; index >= 0; index--) {
-      if (index == stack.size_ - 1) os << "[TOP] ";
-      os << enum2str(stack.data_[index]) << '\n';
+    std::ostream &operator<<(std::ostream &os, const ArrayStack &stack) {
+        os << "size: " << stack.size_ << '\n';
+        if (stack.data_ != nullptr) {
+            // выводим элементы сверху вниз
+            for (int index = stack.size_ - 1; index >= 0; index--) {
+                if (index == stack.size_ - 1) os << "[TOP] ";
+                os << enum2str(stack.data_[index]) << '\n';
+            }
+        }
+        return os;
     }
-  }
-  return os;
-}
 
 }  // namespace itis
--- src/linked_dequeue.cpp
+++ src/linked_dequeue.cpp
@@ -4,66 +4,124 @@
 
 namespace itis {
 
-void LinkedDequeue::Enqueue(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::Enqueue(Element e) {
+        DoublyNode* new_node  = new DoublyNode(e, back_, nullptr);
+        if (size_ != 0) {
+            back_->next = new_node;
+            back_ = new_node;
+        } else {
+            front_ = new_node;
+            back_ = new_node;
+        }
+        size_ ++;
+        // TODO: напишите здесь свой код ...
+    }
 
-void LinkedDequeue::EnqueueFront(Element e) {
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::EnqueueFront(Element e) {
+        // TODO: напишите здесь свой код ...
+        DoublyNode* new_node = new DoublyNode(e, nullptr, front_);
+        if (size_ != 0) {
+            front_->previous = new_node;
+            front_ = new_node;
+        } else {
+            back_ = new_node;
+            front_ = new_node;
 
-void LinkedDequeue::Dequeue() {
-  if (size_ == 0) {
-    throw std::logic_error("cannot not dequeue from empty queue");
-  }
+        }
+        size_ ++;
+    }
 
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::Dequeue() {
+        if (size_ == 0) {
+            throw std::logic_error("cannot not dequeue from empty queue");
+        }
+        if (size_ == 1) {
+            delete front_;
+            front_ = nullptr;
+            back_ = nullptr;
+        } else {
+            DoublyNode* node_to_delete = front_;
+            front_ = front_->next;
+            delete node_to_delete;
+            front_->previous = nullptr;
+        }
+        size_ --;
 
-void LinkedDequeue::DequeueBack() {
-  if (size_ == 0) {
-    throw std::logic_error("cannot not dequeue from empty queue");
-  }
+        // TODO: напишите здесь свой код ...
+    }
 
-  // TODO: напишите здесь свой код ...
-}
+    void LinkedDequeue::DequeueBack() {
+        if (size_ == 0) {
+            throw std::logic_error("cannot not dequeue from empty queue");
+        }
+        if (size_ == 1) {
+            delete back_;
+            front_ = nullptr;
+            back_ = nullptr;
+        } else {
+            DoublyNode *node_to_delete = back_;
+            back_ = back_->previous;
+            delete node_to_delete;
+            back_->next = nullptr;
+        }
+        size_--;
 
-void LinkedDequeue::Clear() {
-  // TODO: напишите здесь свой код ...
-}
+        // TODO: напишите здесь свой код ...
+    }
+
+    void LinkedDequeue::Clear() {
+        // TODO: напишите здесь свой код ...
+        if (size_ == 1) {
+            delete front_;
+        } else {
+            if (size_ != 0) {
+                DoublyNode *current_node = front_;
+                DoublyNode *next_node = front_->next;
+                delete current_node;
+                for (int i = 0; i < size_ - 1; i++) {
+                    current_node = next_node;
+                    next_node = next_node->next;
+                    delete current_node;
+                }
+            }
+        }
+        size_ = 0;
+        front_ = nullptr;
+        back_ = nullptr;
+    }
 
 // === РЕАЛИЗОВАНО ===
 
-LinkedDequeue::~LinkedDequeue() {
-  Clear();
-}
+    LinkedDequeue::~LinkedDequeue() {
+        Clear();
+    }
 
-std::optional<Element> LinkedDequeue::front() const {
-  return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
-}
+    std::optional<Element> LinkedDequeue::front() const {
+        return front_ == nullptr ? std::nullopt : std::make_optional(front_->data);
+    }
 
-std::optional<Element> LinkedDequeue::back() const {
-  return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
-}
+    std::optional<Element> LinkedDequeue::back() const {
+        return back_ == nullptr ? std::nullopt : std::make_optional(back_->data);
+    }
 
-bool LinkedDequeue::IsEmpty() const {
-  return size_ == 0;
-}
+    bool LinkedDequeue::IsEmpty() const {
+        return size_ == 0;
+    }
 
-int LinkedDequeue::size() const {
-  return size_;
-}
+    int LinkedDequeue::size() const {
+        return size_;
+    }
 
 // === НЕОБХОДИМО ДЛЯ ТЕСТИРОВАНИЯ ===
 
-std::ostream &operator<<(std::ostream &os, const LinkedDequeue &queue) {
-  os << "size: " << queue.size_ << '\n';
-  for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->previous) {
-    if (current_node == queue.front_) os << "[FRONT] ";
-    if (current_node == queue.back_) os << "[BACK] ";
-    os << enum2str(current_node->data) << '\n';
-  }
-  return os;
-}
+    std::ostream &operator<<(std::ostream &os, const LinkedDequeue &queue) {
+        os << "size: " << queue.size_ << '\n';
+        for (auto current_node = queue.front_; current_node != nullptr; current_node = current_node->previous) {
+            if (current_node == queue.front_) os << "[FRONT] ";
+            if (current_node == queue.back_) os << "[BACK] ";
+            os << enum2str(current_node->data) << '\n';
+        }
+        return os;
+    }
 
 }  // namespace itis
